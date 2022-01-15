32a756fa91aa1b935fc746dfc40302f104fa1e16
piraces <raul.piraces@gmail.com>
2022-01-15 14:42:06 +0100

    Improve pipelines

--- scripts/generateFirefoxExtension.sh
+++ scripts/generateFirefoxExtension.sh
@@ -11,4 +11,4 @@ echo $(jq -s 'reduce .[] as $item ({}; . * $item)' manifest.json part.firefox.js
 rm -rf assets && rm -rf .github && rm -rf .git && rm .gitignore package-lock.json package.json part.edge.json part.firefox.json CHANGELOG.md styles/main.css styles/main.css.map styles/main.scss styles/popup.css styles/popup.css.map styles/popup.scss
 find ./node_modules/ ! \( -name "node_modules" -or -name "marked" \) -type d -exec rm -rf {} +
 find ./node_modules ! -name 'marked.min.js' -type f -exec rm -f {} +
-zip -r tldr-pages-edge.xpi .
+zip -r tldr-pages-firefox.xpi .
